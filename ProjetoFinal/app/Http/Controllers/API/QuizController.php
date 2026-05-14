1a762da462ceacf848142232186f5df04030873f
constancafurtado <constancafurtado43@gmail.com>
2026-05-14 21:56:58 +0000

    Atualização do QuizController

---
+++ ProjetoFinal/app/Http/Controllers/API/QuizController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\Quiz;
+use Illuminate\Http\Request;
+
+class QuizController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+
+    }
+
+    public function start(Request $request)
+    {
+        $request->validate(['category' => 'required']);
+
+        $questions = Question::Where('category', $request->category)
+        ->with('options')
+        ->inRandomOrder()
+        ->limit(8)
+        ->get();
+
+        $quiz = Quiz::create([
+            'user_id'  =>$request->user()->id,
+            'category' => $request->category,
+        ]);
+
+        return response()->json([
+            'quiz' => $quiz,
+            'questions' => $questions
+        ]);
+    }
+
+    public function submit(Request $request, Quiz $quiz)
+    {
+        $request->validate([
+           'answer' => 'required|array',
+            'answer.*.question_id' => 'required|exists:questions,id',
+            'answer.*.option_id' => 'required|exists:options,id',
+        ]);
+
+        $score = 0;
+
+        foreach ($request->answer as $answer) {
+            $option    = Option::find($answer['option_id']);
+            $is_correct = $option->is_correct;
+
+            if ($is_correct) {
+                $score ++ ; //calcula +1 ponto a cada resposta que acertarmos
+            }
+
+            QuizAnswer::create([
+               'quiz_id'     => $quiz->id,
+                'question_id'=> $answer['question_id'],
+                'option_id'  => $answer['option_id'],
+                'is_correct' => $is_correct,
+            ]);
+
+            //guarda a pontuaçao e delvolve o quiz como respondido
+            $quiz->update(['score' => $score, 'completed_at' => now();
+        }
+
+        return response()->json([
+            'score' => $score,
+            'total' => 8
+        ]);
+
+    }
+
+    // mostra os quizzes já resolvidos pelo utilizador
+    public function history(Request $request)
+    {
+        $quizzes = Quiz::Where('user_id', $request->user()->id)
+            ->with('category')
+            ->Where('completed_at', '!=', null) // diferente de null ou seja os quizzes que já estão complerados
+            ->get();
+
+        return response()->json($quizzes);
+    }
+
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Quiz $quiz) {
+        $quiz->load(['answers.question', 'answers.option', 'category']);
+        return response()->json($quiz);
+    }
+
+
+}
--- ProjetoFinal/app/Models/Quiz.php
+++ ProjetoFinal/app/Models/Quiz.php
@@ -3,8 +3,26 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Quiz extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'body', 'category_id', 'pontuacao', 'completed_at'
+    ];
+
+    pubblic function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+       return $this->belongsTo(Category::class);
+    }
+
+    public function answers()
+    {
+        return $this->belongsTo(Answers::class);
+    }
 }
